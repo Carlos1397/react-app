6a26364aabce8e731235c7b754037cfdd637657e
CARLOS VIERA <pisis_13_3@hotmail.com>
2022-01-15 12:22:37 -0500

    Modificando texto

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <div>
       <h1>Mi primera aplicación</h1>
-      <p>Esta es mi priemra aplicación en React, está padrísimo!</p>
-      </div>   
+      <p>Esta es mi primera aplicación en React, está padrísimo!</p>
+      </div>
+   
     </div>
   );
 }
