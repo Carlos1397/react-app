d1f990b40aa03768be0744f83e28fb20e552412a
CARLOS VIERA <pisis_13_3@hotmail.com>
2022-01-15 12:07:51 -0500

    Cambio de texto en el parrafo App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Mi primera aplicación</h1>
-      <p>Esta es mi aplicación en React, está padrísimo!</p>
+      <p>Esta es mi priemra aplicación en React, está padrísimo!</p>
     </div>
   );
 }
