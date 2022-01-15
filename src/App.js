4f1b03b69fa11ec1935bb015260ff2ffc943a5b1
CARLOS VIERA <pisis_13_3@hotmail.com>
2022-01-15 12:19:18 -0500

    Agregando un div a App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div>
       <h1>Mi primera aplicación</h1>
       <p>Esta es mi priemra aplicación en React, está padrísimo!</p>
+      </div>   
     </div>
   );
 }
