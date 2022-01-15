f1398314b3ee1221aa3df8ace7ccac49cce7b7a7
CARLOS VIERA <pisis_13_3@hotmail.com>
2022-01-15 11:57:16 -0500

    Creando el proyecto, primer commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mi primera aplicación</h1>
+      <p>Esta es mi aplicación en React, está padrísimo!</p>
+    </div>
+  );
+}
+
+export default App;
